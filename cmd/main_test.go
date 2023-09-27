86a2fbc174929fe8cef9c557e06119bd9ead89f5
Nick Loadholtes <nick@ironboundsoftware.com>
2023-09-27 13:48:30 -0400

    Adding a test that might be redundant

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -19,3 +19,18 @@ func TestCommandFromString(t *testing.T) {
 	}
 
 }
+
+func TestCommandFromStringSlice(t *testing.T) {
+	cmd_obj, err := CommandFromStringSlice([]string{"ls", "-la"})
+	if err != nil {
+		t.Fatal("CommandFromString errored out: ", err)
+	}
+	// I really do not like this, I did not specify that path yet it derives it?
+	if !strings.HasSuffix(cmd_obj.Path, "ls") {
+		t.Fatal("cmd_obj is not ls. Saw: ", cmd_obj.Path)
+	}
+	if cmd_obj.Args[1] != "-la" {
+		t.Fatal("wrong arguments for ls seen: ", cmd_obj.Args)
+	}
+
+}
